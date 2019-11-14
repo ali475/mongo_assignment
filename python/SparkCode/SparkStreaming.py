0c42f553050caed4c0fe3e69df3a60725974ac24
ali <alialimohamed475@gmail.com>
2019-11-14 09:57:58 +0200

    return to the old code

--- python/SparkCode/SparkStreaming.py
+++ python/SparkCode/SparkStreaming.py
@@ -1,26 +1,48 @@
-import sys
-from pyspark.sql.functions import udf
+import argparse
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
 
 
-if __name__ == "__main__":
-    spark = SparkSession \
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    # INPUT DETAILS
+    parser.add_argument("--topic_name", type=str, required=True)
+    parser.add_argument('--ZK_opt', type=str, required=True)
+    parser.add_argument('--mongo_sever', type=str, required=True)
+    return parser.parse_args()
+
+
+def open_streaming(topic, ZK_opt):
+    sc = SparkContext(appName="PythonStreamingRecieverKafkaWordCount")
+
+
+def open_streaming(topic, ZK_opt, mongo_host):
+    my_spark = SparkSession \
         .builder \
-        .appName("StructuredNetworkWordCount") \
-        .config("spark.mongodb.input.uri", "mongodb://10.128.0.19:27017/twitter.test") \
-        .config("spark.mongodb.output.uri", "mongodb://10.128.0.19:27017/twitter.test") \
+        .appName("myApp") \
+        .config("spark.mongodb.input.uri", "mongodb://{}/sparkkafka.stream".format(mongo_host)) \
         .getOrCreate()
-    events = spark \
-        .readStream \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", "10.128.0.19:9092,10.128.0.16:9092,10.128.0.20:9092") \
-        .option("subscribe", "testo") \
-        .load()
-    events = events.selectExpr("CAST(value as String)")
+    sc = my_spark.sparkContext
+    ssc = StreamingContext(sc, 2)
+    kvs = KafkaUtils.createStream(ssc, ZK_opt, 'spark-streaming', {topic: 1})
+    return kvs, sc, ssc
 
-    mongooutput = events \
-        .writeStream \
-        .format("com.mongodb.spark.sql.DefaultSource") \
-        .option("com.mongodb.spark.sql.DefaultSource", "mongodb://localhost:27017/twitter.test") \
-        .start()
-    mongooutput.awaitTermination()
+
+def printing(word):
+    print(word)
+    return word
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    kvs, sc, ssc = open_streaming(args.topic_name, args.ZK_opt)
+    kvs, sc, ssc = open_streaming(args.topic_name, args.ZK_opt, args.mongo_server)
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+    counts.map(lambda word: printing(word))
+    counts.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
