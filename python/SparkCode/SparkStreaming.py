ac6d8803a6f3f1a237864cb34c180e360dca1e09
ali <alialimohamed475@gmail.com>
2019-11-17 15:23:35 +0200

    adding new code to read kafka testo topic

--- python/SparkCode/SparkStreaming.py
+++ python/SparkCode/SparkStreaming.py
@@ -1,48 +1,25 @@
-import argparse
+import findspark
+
+findspark.init('/opt/spark')
+import os
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--jars ~/spark-streaming-kafka-0-8-assembly_2.11-2.4.4 pyspark-shell'
+
+import sys
+import time
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    # INPUT DETAILS
-    parser.add_argument("--topic_name", type=str, required=True)
-    parser.add_argument('--ZK_opt', type=str, required=True)
-    parser.add_argument('--mongo_sever', type=str, required=True)
-    return parser.parse_args()
-
-
-def open_streaming(topic, ZK_opt):
-    sc = SparkContext(appName="PythonStreamingRecieverKafkaWordCount")
-
-
-def open_streaming(topic, ZK_opt, mongo_host):
-    my_spark = SparkSession \
-        .builder \
-        .appName("myApp") \
-        .config("spark.mongodb.input.uri", "mongodb://{}/sparkkafka.stream".format(mongo_host)) \
-        .getOrCreate()
-    sc = my_spark.sparkContext
-    ssc = StreamingContext(sc, 2)
-    kvs = KafkaUtils.createStream(ssc, ZK_opt, 'spark-streaming', {topic: 1})
-    return kvs, sc, ssc
-
-
-def printing(word):
-    print(word)
-    return word
-
-
-if __name__ == '__main__':
-    args = parse_arguments()
-    kvs, sc, ssc = open_streaming(args.topic_name, args.ZK_opt)
-    kvs, sc, ssc = open_streaming(args.topic_name, args.ZK_opt, args.mongo_server)
-    lines = kvs.map(lambda x: x[1])
-    counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-    counts.map(lambda word: printing(word))
-    counts.pprint()
-
-    ssc.start()
-    ssc.awaitTermination()
+n_secs = 1
+topic = "testo"
+conf = SparkConf().setAppName("KafkaStreamProcessor")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("WARN")
+ssc = StreamingContext(sc, n_secs)
+kafkaStream = KafkaUtils.createDirectStream(ssc, [topic], {
+    'bootstrap.servers': '10.128.0.20:9092,10.128.0.16:9092,10.128.0.19:9092'})
+lines = kafkaStream.map(lambda x: x[1])
+counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+counts.pprint()
+ssc.start()
